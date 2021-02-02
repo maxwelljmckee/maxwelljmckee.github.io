0b14390b9a3ed8bdec6d4e426b90ba127b0bdbd5
maxwelljmckee <maxwelljmckee@gmail.com>
2021-02-02 11:16:22 -0700

    modify Hero

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -10,6 +10,7 @@ const Header = () => {
 
   const [isDesktop, setIsDesktop] = useState(false);
   const [isMobile, setIsMobile] = useState(false);
+  const [show, setShow] = useState(false);
 
   useEffect(() => {
     if (window.innerWidth > 769) {
@@ -19,30 +20,38 @@ const Header = () => {
       setIsMobile(true);
       setIsDesktop(false);
     }
+    setTimeout(() => {
+      setShow(true)
+    }, 500)
   }, []);
 
+
   return (
-    <section id="hero" className="jumbotron">
-      <Container>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
-          <h1 className="hero-title">
-            {title || 'Hi, my name is'}{' '}
-            <span className="text-color-main">{name || 'Your Name'}</span>
-            <br />
-            {subtitle || ""}
-          </h1>
-        </Fade>
-        <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
-          <p className="hero-cta">
-            <span className="cta-btn cta-btn--hero">
-              <Link to="about" smooth duration={1000}>
-                {cta || 'Know more'}
-              </Link>
-            </span>
-          </p>
-        </Fade>
-      </Container>
-    </section>
+    <>
+      { show && 
+        <section id="hero" className="jumbotron">
+          <Container>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={500} distance="30px">
+              <h1 className="hero-title">
+                {title || 'Hi, my name is'}{' '}
+                <span className="text-color-main">{name || ''}</span>
+                <br />
+                {subtitle || ""}
+              </h1>
+            </Fade>
+            <Fade left={isDesktop} bottom={isMobile} duration={1000} delay={1000} distance="30px">
+              <p className="hero-cta">
+                <span className="cta-btn cta-btn--hero">
+                  <Link to="about" smooth duration={1000}>
+                    {cta || 'Learn more'}
+                  </Link>
+                </span>
+              </p>
+            </Fade>
+          </Container>
+        </section>
+      }
+    </>
   );
 };
 
